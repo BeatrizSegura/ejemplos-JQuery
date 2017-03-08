7c2b4211502f4d72b291bb7d0cad7be5a180a4fe
BeatrizSegura <betty12_19@hotmail.com>
2017-03-08 15:42:02 -0600

    jQuery. Se agregaron los cambios en el archivo html y se agrego un archivo js

---
+++ holajquery.js
@@ -0,0 +1,9 @@
+// $(document).ready(saluda)
+// function saluda(){
+//     alert($('h1').text())
+// }
+$(function(){
+    //alert($('h1').text())
+    $nuevo=$('<p> Parrafo <b>Nuevo</b> </p>')   //Se crea un Parrafo nuevo en un objeto jQuery
+    $('h2').after($nuevo)
+})
